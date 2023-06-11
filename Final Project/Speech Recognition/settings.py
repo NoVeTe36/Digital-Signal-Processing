afd635a43378b9422ad3772dbf52d4c6c4ea9a64
NoVeTe36 <nguyenviettungntl3006@gmail.com>
2023-06-11 08:54:09 +0700

    Update process

---
+++ Final Project/Speech Recognition/settings.py
@@ -0,0 +1,15 @@
+NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+NUMBERS_FOLDER = "./raw_audio/"
+
+AUDIO_FORMAT = ".wav"
+
+PEOPLE = ["tt", "vt", "mt", "qs", "qh", "qt"]
+
+PROCESSED_FOLDER = "./processed_audio/"
+
+SPECTROGRAM_FOLDER = "./spectrogram/"
+
+FILTER_FOLDER = "./filter_audio/"
+
+FINAL_FOLDER = "./final_audio/"
